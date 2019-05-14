d819c473c3c43073043db7d54bc045fc178e5588
Soaresjh <soaresjh1@gmail.com>
2019-05-14 16:33:28 -0300

    house price prediction

---
+++ house_price/house_prices.py
@@ -0,0 +1,20 @@
+from sklearn.base import TransformerMixin 
+from pickle import load
+
+import numpy as np
+import pandas as pd
+
+
+def to_list_obj(DataFrame, do_print = False):
+    features = DataFrame.dtypes[DataFrame.dtypes==object].index
+    features = list(features)
+    if do_print:
+        print('Object Features: ', *features, sep = '\n-')
+    return [features, len(features)]
+
+def to_list_miss(DataFrame, do_print = False):
+    features = DataFrame.isna().sum()[DataFrame.isna().sum()>0].index
+    features = list(features)
+    if do_print:
+        print('Missing Features: ', *features, sep = '\n-')
+    return [features, len(features)]
